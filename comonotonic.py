2945892b479e271d1c8adcc19b1c0ef9470b6604
PengCheng <pc980912@gmail.com>
2020-01-22 14:34:00 +0800

    modification on weighted avg method

--- comonotonic.py
+++ comonotonic.py
@@ -3,13 +3,16 @@ import numpy as np
 import pandas as pd
 import copy
 import operator
+import utils
+from sklearn.model_selection import train_test_split
 
 # handle the case of pure comonotonicity
-class pure_comonotonic():
+class pure_comonotonic:
     def __init__(self, x_train, y_train, unrankable):
         """
-        Features types: discrete rankable; binary; discrete unrankable
+        Features types: discrete rankable; discrete unrankable; continuous
         unrankable should be a list specifying the columns for discrete unrankable features
+        x_train should be categorized before passing in
         """
         self.x_train = x_train # x_train is numpy 2d array
         self.y_train = y_train # y_train is numpy 1d array
@@ -132,6 +135,12 @@ class pure_comonotonic():
         
     def predict_single(self, x):
         # deal with a single case
+        prob_distribution = self.get_prob_dist_single(x)
+        predicted_class = max(prob_distribution.items(), key=operator.itemgetter(1))[0]
+        return predicted_class
+    
+    def get_prob_dist_single(self, x):
+        # get the probability distribution of one instance
         prob_distribution = self.prior_prob.copy() # initialize with prior probability
         if self.unrankable != None:
             for c in prob_distribution.keys():
@@ -144,8 +153,11 @@ class pure_comonotonic():
                 fv = x[f]
                 intervals.append(self.como_prob_interval[f][c][fv])
             prob_distribution[c] *= self.interval_intersection(intervals)
-        predicted_class = max(prob_distribution.items(), key=operator.itemgetter(1))[0]
-        return predicted_class
+        prob_distribution_list = list(prob_distribution.values())
+        prob_distribution_list = [i*(10**8) for i in prob_distribution_list]
+        for k in prob_distribution.keys():
+            prob_distribution[k] = prob_distribution[k]*(10**8)/sum(prob_distribution_list) 
+        return prob_distribution        
             
     def predict(self, x_test):
         y_predict = []
@@ -153,10 +165,161 @@ class pure_comonotonic():
             y_predict.append(self.predict_single(x))
         return y_predict
 
+class clustered_comonotonic(pure_comonotonic):
+    def __init__(self, x_train, y_train, unrankable, uncategorized_df, colnames, min_corr, random_state):
+        super(clustered_comonotonic, self).__init__(x_train, y_train, unrankable)
+        # compute the correlation coefficient matrix by uncategorized data, include unrankable features
+        self.uncategorized_df = uncategorized_df
+        self.colnames = colnames
+        self.min_corr = min_corr
+        self.random_state = random_state
+    
+    def clustering(self):
+        # here we need to make sure that we compute the correlation coefficient matrix by the
+        # uncategorized X in the training set
+        uncategorized_x = self.uncategorized_df[self.colnames[:-1]].to_numpy()
+        uncategorized_y = self.uncategorized_df[self.colnames[-1]].to_numpy()
+        x_corr_train, x_corr_test, y_corr_train, y_corr_test = train_test_split(uncategorized_x,
+                                                                                uncategorized_y, test_size = 0.2, 
+                                                                                random_state = self.random_state)
+        del x_corr_test
+        del y_corr_train
+        del y_corr_test
+        rankable_var = np.array([[x_corr_train[row][col] for col in self.como] for row in range(len(x_corr_train))])
+        corr_matrix = np.corrcoef(rankable_var.T)
+        abs_corr = np.absolute(corr_matrix)
+        # need to do clustering by corr_matrix
+        distance_matrix = 1 - abs_corr
+        cluster_book = utils.cluster_agnes(distance_matrix, 1-self.min_corr)
+        self.cluster_book = cluster_book # list of list
+    
+    def run(self):
+        self.get_prior_prob()
+        self.extract_feature_val()
+        if self.unrankable != None:
+            self.get_unrankable_prob()
+        self.get_comonotonic_prob_interval()
+        self.clustering()
+    
+    def predict_single(self, x):
+        prob_distribution = self.get_prob_dist_single(x)
+        predicted_class = max(prob_distribution.items(), key=operator.itemgetter(1))[0]
+        return predicted_class
+    
+    def get_prob_dist_single(self, x):
+        # get the probability distribution of one instance
+        prob_distribution = self.prior_prob.copy() # initialize with prior probability
+        if self.unrankable != None:
+            for c in prob_distribution.keys():
+                for f in self.unrankable:
+                    fv = x[f] # fv stands for feature value
+                    prob_distribution[c] *= self.unrankable_post_prob[f][c][fv]
+        for c in prob_distribution.keys():
+            for cluster in self.cluster_book:
+                interval = []
+                for f_idx in cluster:
+                    f = self.como[f_idx]
+                    fv = x[f]
+                    interval.append(self.como_prob_interval[f][c][fv])
+                prob_distribution[c] *= self.interval_intersection(interval)
+        prob_distribution_list = list(prob_distribution.values())
+        prob_distribution_list = [i*(10**8) for i in prob_distribution_list]
+        for k in prob_distribution.keys():
+            prob_distribution[k] = prob_distribution[k]*(10**8)/sum(prob_distribution_list) 
+        return prob_distribution
+        
 
-def accuracy(y_predict, y_test):
-    t = 0
-    for i in range(len(y_test)):
-        if y_predict[i] == y_test[i]:
-            t += 1        
-    return t/len(y_test)
+class naive_bayes:
+    # note that the x_train here SHOULD NOT BE CATEGORIZED, pass in after encoding
+    # cont_feature is a list containing indices of continuous features
+    def __init__(self, x_train, y_train, cont_feature):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.cont_feature = cont_feature
+        self.discrete_feature = list(set([i for i in range(len(x_train[0]))]) - set(cont_feature))
+    
+    def get_prior_prob(self):
+        # get the prior probability and indices of instances for different classes
+        prior_prob = {} # key is class, value is the prior probability of this class
+        class_idx = {} # key is class, value is a list containing the indices of instances for this class
+        for i in range(len(self.y_train)):
+            if self.y_train[i] not in class_idx.keys():
+                class_idx[self.y_train[i]] = [i]
+            else:
+                class_idx[self.y_train[i]].append(i)
+        prior_prob = {k:len(v)/len(self.y_train) for k,v in class_idx.items()}
+        self.prior_prob = prior_prob
+        self.class_idx = class_idx
+
+    # extract the discrete feature value
+    def get_discrete_feature_val(self):
+        discrete_feature_val = {}
+        for i in self.discrete_feature:
+            discrete_feature_val[i] = max(np.unique(self.x_train.T[i]))+1
+        self.discrete_feature_val = discrete_feature_val # how many categories for each feature
+
+    def get_post_prob(self):
+        discrete_post_prob = {} # { feature: {class: {fv: post_prob } } }
+        for f in self.discrete_feature:
+            feature_dict = {}
+            for c in self.class_idx.keys():
+                class_dict = {}
+                for idx in self.class_idx[c]: # traverse the instances of this class
+                    if self.x_train[idx][f] not in class_dict.keys():
+                        class_dict[self.x_train[idx][f]] = 1
+                    else:
+                        class_dict[self.x_train[idx][f]] += 1
+                # use Laplacian correction to avoid zero probability problem
+                all_fv = [i for i in range(self.discrete_feature_val[f])]
+                for fv in all_fv:
+                    if fv not in class_dict.keys():
+                        class_dict[fv] = 1
+                summation = sum(class_dict.values())
+                class_dict = {k:v/summation for k,v in class_dict.items()}
+                feature_dict[c] = class_dict
+            discrete_post_prob[f] = feature_dict
+        # guassian distribution for continuous features
+        # we store the mean and standard deviation for each feature within each class
+        cont_mean_sd = {} # { feature: {class: (mean, sd)} }
+        for f in self.cont_feature:
+            feature_dict = {}
+            for c in self.class_idx.keys():
+                values = []
+                for idx in self.class_idx[c]:
+                    values.append(self.x_train[idx][f])
+                mean = np.mean(np.array(values))
+                sd = np.std(np.array(values))
+                feature_dict[c] = (mean, sd)
+            cont_mean_sd[f] = feature_dict
+        self.discrete_post_prob = discrete_post_prob
+        self.cont_mean_sd = cont_mean_sd
+
+    def run(self):
+        self.get_prior_prob()
+        self.get_discrete_feature_val()
+        self.get_post_prob()
+
+    def predict_single(self, x):
+        # deal with a single case
+        prob_distribution = self.prior_prob.copy() # initialize with prior probability
+        for c in prob_distribution.keys():
+            for f in self.discrete_feature:
+                fv = x[f] # fv stands for feature value
+                prob_distribution[c] *= self.discrete_post_prob[f][c][fv]
+        for c in prob_distribution.keys():
+            for f in self.cont_feature:
+                fv = x[f]
+                prob_density = utils.gaussian_pdf(self.cont_mean_sd[f][c],fv)
+                prob_distribution[c] *= prob_density
+        predicted_class = max(prob_distribution.items(), key=operator.itemgetter(1))[0]
+        prob_distribution_list = list(prob_distribution.values())
+        prob_distribution_list = [i*(10**24) for i in prob_distribution_list]
+        for k in prob_distribution.keys():
+            prob_distribution[k] = prob_distribution[k]*(10**24)/sum(prob_distribution_list) 
+        return predicted_class, prob_distribution
+            
+    def predict(self, x_test):
+        y_predict = []
+        for x in x_test:
+            y_predict.append(self.predict_single(x)[0])
+        return y_predict
--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import copy
 import comonotonic as cm
+from sklearn.model_selection import train_test_split
+import random
+import math
+import operator
 
 # given a list containing text values in a column
 # encode to 0, 1, 2...
@@ -26,22 +30,166 @@ def encode_df(df, encoded_columns):
             df_copy['X'+str(col)] = encoded
     return df_copy
 
+
 # to test the accuracy when a certain allocation of number of categories is employed 
-def accuracy_test(allocation_book, df, unrankable): # allocation book is {feature_idx: num_of_categories}; unrankable is a list
+def accuracy_test(allocation_book, df, unrankable, colnames, min_corr, use_cluster, random_state): 
+    # allocation book is {feature_idx: num_of_categories}; unrankable is a list
+    # df is the encoded but NOT categorized. DO NOT CHANGE df!!!
+    if use_cluster == True:
+        uncategorized_df = df.copy()
+    df_for_categorize = df.copy()
+    for col_idx in allocation_book.keys():
+        discretized_col = pd.cut(df_for_categorize.iloc[:,col_idx],allocation_book[col_idx], labels=[i for i in range(allocation_book[col_idx])])
+        df_for_categorize['X'+str(col_idx)] = discretized_col
+    # For pure comonotonic classifier
+    X = df_for_categorize[colnames[:-1]].to_numpy()
+    Y = df_for_categorize[colnames[-1]].to_numpy()
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=random_state)
+    if use_cluster == False: # use pure comonotonic
+        pure_como_classifier = cm.pure_comonotonic(X_train, Y_train, unrankable = unrankable)
+        pure_como_classifier.run()
+        Y_predict = pure_como_classifier.predict(X_test)
+    else: # use clustered comonotonic
+        #uncategorized_x = uncategorized_df[colnames[:-1]].to_numpy()
+        cluster_como_classifier = cm.clustered_comonotonic(X_train, Y_train, unrankable = unrankable,
+                                                        uncategorized_df = uncategorized_df, colnames = colnames,
+                                                        min_corr = min_corr, random_state = random_state)
+        cluster_como_classifier.run()
+        Y_predict = cluster_como_classifier.predict(X_test)
+    accuracy = get_accuracy(Y_predict, Y_test)
+    return accuracy
+
+
+"""
+This is actually a mistaken accuracy test because it changes the value of df after the accuracy test
+However, after iterations the accuracy of pure comonotonic rises up for adult.csv dataset
+"""
+def mistaken_accuracy_test(allocation_book, df, unrankable, colnames, min_corr, use_cluster, random_state): 
+    # allocation book is {feature_idx: num_of_categories}; unrankable is a list
+    # df is the encoded but NOT categorized. DO NOT CHANGE df!!!
+    if use_cluster == True:
+        uncategorized_df = df.copy()
     for col_idx in allocation_book.keys():
         discretized_col = pd.cut(df.iloc[:,col_idx],allocation_book[col_idx], labels=[i for i in range(allocation_book[col_idx])])
-    df['X'+str(col_idx)] = discretized_col
+        df['X'+str(col_idx)] = discretized_col
     # For pure comonotonic classifier
     X = df[colnames[:-1]].to_numpy()
     Y = df[colnames[-1]].to_numpy()
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=10)
-    pure_como_classifier = cm.pure_comonotonic(X_train, Y_train, unrankable = unrankable)
-    pure_como_classifier.run()
-    Y_predict = pure_como_classifier.predict(X_test)
-    accuracy = cm.accuracy(Y_predict, Y_test)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=random_state)
+    if use_cluster == False: # use pure comonotonic
+        pure_como_classifier = cm.pure_comonotonic(X_train, Y_train, unrankable = unrankable)
+        pure_como_classifier.run()
+        Y_predict = pure_como_classifier.predict(X_test)
+    else: # use clustered comonotonic
+        #uncategorized_x = uncategorized_df[colnames[:-1]].to_numpy()
+        cluster_como_classifier = cm.clustered_comonotonic(X_train, Y_train, unrankable = unrankable,
+                                                        uncategorized_df = uncategorized_df, colnames = colnames,
+                                                        min_corr = min_corr, random_state = random_state)
+        cluster_como_classifier.run()
+        Y_predict = cluster_como_classifier.predict(X_test)
+    accuracy = get_accuracy(Y_predict, Y_test)
     return accuracy
+
 
 # to check which combination of allocations is the best
 # i.e. for each continuous feature, determine how many categories we should divide
-def determine_allocation(cont_col, num_categories_list):
+# the idea is from simulated annealing algorithm
+# cont_col is a list of features which are continuous, num_categories_list is a list of all possible number of categories
+# suppose cont_col has size m, num_categories_list has size n, the search space has size n^m
+def determine_allocation(cont_col, num_categories_list, df, unrankable,
+                        colnames, max_itr, temp, anneal_schedule, use_cluster,
+                        use_mistaken_accuracy_test, random_state, min_corr = 0.5):
+    #allocation_book = {f:num_categories_list[0] for f in cont_col}
+    allocation_book = {f:random.choice(num_categories_list) for f in cont_col}
+    allocation_history = {}
+    accuracy_history = {}
+    for itr in range(max_itr):
+        allocation_history[itr] = allocation_book
+        if use_mistaken_accuracy_test == True:
+            old_accuracy = mistaken_accuracy_test(allocation_book, df, unrankable, colnames, min_corr, use_cluster, random_state)
+        else:
+            old_accuracy = accuracy_test(allocation_book, df, unrankable, colnames, min_corr, use_cluster, random_state)
+        accuracy_history[itr] = old_accuracy
+        
+        if (itr+1)%anneal_schedule == 0:
+            temp *= 0.9
+        update_feature = cont_col[itr%len(cont_col)]
+        old_value = allocation_book[update_feature]
+        update_value = random.choice(num_categories_list)
+        while update_value == old_value:
+            update_value = random.choice(num_categories_list)
+        candidate = allocation_book.copy()
+        candidate[update_feature] = update_value
+        if use_mistaken_accuracy_test == True:
+            new_accuracy = mistaken_accuracy_test(candidate, df, unrankable, colnames, min_corr, use_cluster, random_state)
+        else:
+            new_accuracy = accuracy_test(candidate, df, unrankable, colnames, min_corr, use_cluster, random_state)
+        
+        if new_accuracy > old_accuracy:
+            allocation_book = candidate.copy()
+        else:
+            transition_prob = math.exp((new_accuracy - old_accuracy)/temp)
+            determinant = np.random.uniform(0, 1)
+            if determinant < transition_prob:
+                allocation_book = candidate.copy()
+    return accuracy_history, allocation_history
+
+def get_accuracy(y_predict, y_test):
+    t = 0
+    for i in range(len(y_test)):
+        if y_predict[i] == y_test[i]:
+            t += 1        
+    return t/len(y_test)
+
+######## for clustering by correlation coefficient matrix
+# c1, c2 are two lists, compute the distance between them
+def get_distance(distance_matrix, c1, c2):
+    min_dist = None
+    for f1 in c1:
+        for f2 in c2:
+            dist = distance_matrix[f1][f2]
+            if min_dist == None or dist < min_dist:
+                min_dist = dist
+    return min_dist
+
+# update cluster_book
+def update_cluster_book(distance_matrix, cluster_book):
+    min_dist = None
+    merger1 = None
+    merger2 = None
+    for c1 in cluster_book:
+        for c2 in cluster_book:
+            if c1 != c2:
+                dist = get_distance(distance_matrix, c1, c2)
+                if min_dist == None or min_dist > dist:
+                    merger1 = c1.copy()
+                    merger2 = c2.copy()
+                    min_dist = dist
+    cluster_book.remove(merger1)
+    cluster_book.remove(merger2)
+    cluster_book.append(merger1+merger2)
+    return cluster_book, min_dist
+
+# stopping condition: the current minimum distance among clusters larger than max_distance
+def cluster_agnes(distance_matrix, max_distance):
+    cluster_book = [[i] for i in range(len(distance_matrix))] # list of list, store the current clusters
+    cluster_book_copy = cluster_book.copy() # in case clustering is unnecessary
+    cluster_book, min_dist = update_cluster_book(distance_matrix, cluster_book) 
+    cluster_necessary = False # if clustering is unnecessary, just use cluster_book_copy
+    while min_dist <= max_distance:
+        cluster_necessary = True
+        cluster_book, min_dist = update_cluster_book(distance_matrix, cluster_book)
+    if cluster_necessary == False:
+        return cluster_book_copy
+    else:
+        return cluster_book
     
+def gaussian_pdf(mean_sd_tuple, x):
+    mean = mean_sd_tuple[0]
+    sd = mean_sd_tuple[1]
+    return (math.exp(-0.5*(( (x-mean) / sd)**2)))/(sd*math.sqrt(2 * math.pi))
+
+# use weighted average of naive bayes and pure comonotonic
+def weighted_avg(nb_dist, p_como_dist, w_nb):
+    prob_dist = {c: (nb_dist[c]*w_nb+p_como_dist[c]*(1-w_nb)) for c in nb_dist.keys()}
+    return max(prob_dist.items(), key=operator.itemgetter(1))[0]
